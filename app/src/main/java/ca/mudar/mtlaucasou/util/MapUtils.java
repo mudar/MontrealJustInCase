63de7444b46280272c7cc6d535b0afed67486dc4
Mudar Noufal <mn@mudar.ca>
2016-08-30 10:45:28 -0400

    Updated MapUtils
    enableMyLocation, addFeatureCollectionMarkers, getDefaultBounds

--- app/src/main/java/ca/mudar/mtlaucasou/util/MapUtils.java
+++ app/src/main/java/ca/mudar/mtlaucasou/util/MapUtils.java
@@ -23,17 +23,41 @@
 
 package ca.mudar.mtlaucasou.util;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.TextUtils;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import ca.mudar.mtlaucasou.Const;
 import ca.mudar.mtlaucasou.R;
+import ca.mudar.mtlaucasou.model.geojson.PointsFeature;
+import ca.mudar.mtlaucasou.model.geojson.PointsFeatureCollection;
+import ca.mudar.mtlaucasou.model.geojson.base.GeometryFeatureCollection;
 
 public class MapUtils {
+
+    public static void enableMyLocation(AppCompatActivity activity, GoogleMap map) {
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(activity, Const.RequestCodes.LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (map != null) {
+            // Access to the location has been granted to the app.
+            map.setMyLocationEnabled(true);
+        }
+    }
+
     public static BitmapDescriptor getMarkerIcon(Const.MapTypes type) {
         switch (type) {
             case FIRE_HALLs:
@@ -55,5 +79,40 @@ public class MapUtils {
         return Html.fromHtml(descHtml.replace(name, ""))
                 .toString()
                 .trim();
+    }
+
+    public static int addFeatureCollectionMarkers(GoogleMap map, Const.MapTypes type, GeometryFeatureCollection collection) {
+        final PointsFeatureCollection featureCollection = (PointsFeatureCollection) collection;
+
+        int total = 0;
+        if (featureCollection.getFeatures() != null) {
+
+            for (PointsFeature feature : featureCollection.getFeatures()) {
+                final LatLng latLng = GeoUtils.getCoordsLatLng(feature.getGeometry().getCoordinates());
+                final String title = feature.getProperties().getName();
+                final String desc = MapUtils.getCleanDescription(feature.getProperties().getDescription(), title);
+
+                if (latLng != null && !TextUtils.isEmpty(title)) {
+                    final MarkerOptions markerOptions = new MarkerOptions()
+                            .position(latLng)
+                            .icon(MapUtils.getMarkerIcon(type))
+                            .title(title);
+                    if (!TextUtils.isEmpty(desc)) {
+                        markerOptions.snippet(desc);
+                    }
+                    map.addMarker(markerOptions);
+                    total++;
+                }
+            }
+        }
+
+        return total;
+    }
+
+    public static LatLngBounds getDefaultBounds() {
+        return new LatLngBounds(
+                new LatLng(Const.MAPS_GEOCODER_LIMITS[0], Const.MAPS_GEOCODER_LIMITS[1]), // LowerLeft
+                new LatLng(Const.MAPS_GEOCODER_LIMITS[2], Const.MAPS_GEOCODER_LIMITS[3])  // UpperRight
+        );
     }
 }

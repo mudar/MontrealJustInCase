21bbd84c8cee4db1e388229baeaf4d3ccf08baa7
Mudar Noufal <mn@mudar.ca>
2016-09-01 09:29:30 -0400

    Added Realm library
    To Gradle and App

--- app/src/main/java/ca/mudar/mtlaucasou/MtlAuCasOuApp.java
+++ app/src/main/java/ca/mudar/mtlaucasou/MtlAuCasOuApp.java
@@ -25,7 +25,19 @@ package ca.mudar.mtlaucasou;
 
 import android.app.Application;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class MtlAuCasOuApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // The Realm file will be located in Context.getFilesDir() with name "default.realm"
+        RealmConfiguration config = new RealmConfiguration.Builder(this).build();
+        Realm.setDefaultConfiguration(config);
+        Realm.deleteRealm(config);
+    }
 
     @Deprecated
     public String getUnits() {

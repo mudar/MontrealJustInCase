26f2f16add3e899d5ff85d399dc1aa78890e20e2
Mudar Noufal <mn@mudar.ca>
2016-09-01 10:44:19 -0400

    RealmObject models
    With buider to convert GeoJSON objects

---
+++ app/src/main/java/ca/mudar/mtlaucasou/model/LongitudeLatitude.java
@@ -0,0 +1,89 @@
+/*
+    Montréal Just in Case
+    Copyright (C) 2011  Mudar Noufal <mn@mudar.ca>
+
+    Geographic locations of public safety services. A Montréal Open Data
+    project.
+
+    This file is part of Montréal Just in Case.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ca.mudar.mtlaucasou.model;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import ca.mudar.mtlaucasou.model.geojson.Point;
+import io.realm.RealmObject;
+import io.realm.annotations.Ignore;
+
+public class LongitudeLatitude extends RealmObject {
+    private double lng;
+    private double lat;
+
+    @Ignore
+    LatLng latLng;
+
+    public LongitudeLatitude() {
+        // Empty constructor
+    }
+
+    /**
+     * Builder's constructor
+     *
+     * @param builder
+     */
+    private LongitudeLatitude(Builder builder) {
+        this.lng = builder.lng;
+        this.lat = builder.lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public LatLng getLatLng() {
+        return new LatLng(lat, lng);
+    }
+
+    /**
+     * Build a Realm LongitudeLatitude from GeoJSON Point
+     */
+    public static class Builder {
+        private double lat;
+        private double lng;
+
+        public Builder(Point point) {
+            this.lng = point.getCoordinates().get(0);
+            this.lat = point.getCoordinates().get(1);
+        }
+
+        public LongitudeLatitude build() {
+            return new LongitudeLatitude(this);
+        }
+    }
+}
---
+++ app/src/main/java/ca/mudar/mtlaucasou/model/Placemark.java
@@ -0,0 +1,119 @@
+/*
+    Montréal Just in Case
+    Copyright (C) 2011  Mudar Noufal <mn@mudar.ca>
+
+    Geographic locations of public safety services. A Montréal Open Data
+    project.
+
+    This file is part of Montréal Just in Case.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ca.mudar.mtlaucasou.model;
+
+import ca.mudar.mtlaucasou.Const;
+import ca.mudar.mtlaucasou.model.geojson.PointsFeature;
+import io.realm.RealmObject;
+import io.realm.annotations.Ignore;
+import io.realm.annotations.Index;
+
+public class Placemark extends RealmObject {
+    @Ignore
+    public static final String FIELD_MAP_TYPE = "mapType";
+    @Ignore
+    public static final String FIELD_COORDINATES = "coordinates";
+    @Ignore
+    public static final String FIELD_COORDINATES_LAT = "coordinates.lat";
+    @Ignore
+    public static final String FIELD_COORDINATES_LNG = "coordinates.lng";
+
+    private String id;
+    @Index
+    private String mapType;
+    private PlacemarkProperties properties;
+    private LongitudeLatitude coordinates;
+
+    public Placemark() {
+        // Empty constructor
+    }
+
+    /**
+     * Builder's constructor
+     *
+     * @param builder
+     */
+    private Placemark(Builder builder) {
+        this.id = builder.id;
+        this.mapType = builder.mapType;
+        this.properties = builder.properties;
+        this.coordinates = builder.coordinates;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMapType() {
+        return mapType;
+    }
+
+    public void setMapType(String mapType) {
+        this.mapType = mapType;
+    }
+
+    public PlacemarkProperties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(PlacemarkProperties properties) {
+        this.properties = properties;
+    }
+
+    public LongitudeLatitude getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(LongitudeLatitude coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    /**
+     * Build a Realm Placemark from GeoJSON PointsFeature
+     */
+    public static class Builder {
+        private String id;
+        private String mapType;
+        private PlacemarkProperties properties;
+        private LongitudeLatitude coordinates;
+
+        public Builder(PointsFeature pointsFeature, Const.MapTypes mapType) {
+            this.id = pointsFeature.getId();
+            this.mapType = mapType.toString();
+
+            this.properties = new PlacemarkProperties.Builder(pointsFeature.getProperties())
+                    .build();
+            this.coordinates = new LongitudeLatitude.Builder(pointsFeature.getGeometry())
+                    .build();
+        }
+
+        public Placemark build() {
+            return new Placemark(this);
+        }
+    }
+}

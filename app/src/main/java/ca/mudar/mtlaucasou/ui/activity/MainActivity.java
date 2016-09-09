3a05c8220936f641401f16649bb8d8802416a751
Mudar Noufal <mn@mudar.ca>
2016-09-08 21:28:51 -0400

    .ui.activity package

--- app/src/main/java/ca/mudar/mtlaucasou/ui/activity/MainActivity.java
+++ app/src/main/java/ca/mudar/mtlaucasou/ui/activity/MainActivity.java
@@ -21,7 +21,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package ca.mudar.mtlaucasou.ui;
+package ca.mudar.mtlaucasou.ui.activity;
 
 import android.Manifest;
 import android.content.Intent;

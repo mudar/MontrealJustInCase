bfeee7e05f8896190ff7596a0c7a7d144633e3ee
Mudar Noufal <mn@mudar.ca>
2016-09-09 09:38:50 -0400

    EulaActivity, initial

---
+++ app/src/main/java/ca/mudar/mtlaucasou/ui/activity/EulaActivity.java
@@ -0,0 +1,78 @@
+/*
+    Montréal Just in Case
+    Copyright (C) 2011  Mudar Noufal <mn@mudar.ca>
+
+    Geographic locations of public safety services. A Montréal Open Data
+    project.
+
+    This file is part of Montréal Just in Case.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ca.mudar.mtlaucasou.ui.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import ca.mudar.mtlaucasou.Const;
+import ca.mudar.mtlaucasou.R;
+
+public class EulaActivity extends BaseActivity {
+    private static final String ASSETS_URI = "file:///android_asset/";
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, EulaActivity.class);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_eula);
+
+        loadWebView((WebView) findViewById(R.id.webview));
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    private void loadWebView(WebView v) {
+        // Set basic style
+        v.setBackgroundColor(ContextCompat.getColor(this, R.color.color_background));
+        v.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+        // Open links in external browser
+        v.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+                return true;
+            }
+        });
+
+        // Load HTML content from assets
+        v.loadUrl(ASSETS_URI + Const.LocalAssets.LICENSE);
+    }
+}

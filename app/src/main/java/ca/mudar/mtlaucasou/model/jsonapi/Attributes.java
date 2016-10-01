f7118065bddaafe7d36bac81330adb7ce3c0d3dc
Mudar Noufal <mn@mudar.ca>
2016-10-01 16:46:39 -0400

    Parse HelloApi json
    based on jsonapi v1.0

---
+++ app/src/main/java/ca/mudar/mtlaucasou/model/jsonapi/Attributes.java
@@ -0,0 +1,52 @@
+/*
+    Montréal Just in Case
+    Copyright (C) 2011  Mudar Noufal <mn@mudar.ca>
+
+    Geographic locations of public safety services. A Montréal Open Data
+    project.
+
+    This file is part of Montréal Just in Case.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ca.mudar.mtlaucasou.model.jsonapi;
+
+import java.util.Date;
+
+import ca.mudar.mtlaucasou.model.MapType;
+
+public class Attributes {
+    private String title;
+    private Date created;
+    private Date updated;
+    private String type;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    @MapType
+    public String getType() {
+        return type;
+    }
+}
